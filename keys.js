3ee2cf077f21916b813e0800c6ee6a1144b5b814
lydianyagitari <lydiainnocents@gmail.com>
2018-11-30 13:47:21 -0600

    Update keys.js

--- keys.js
+++ keys.js
@@ -2,6 +2,6 @@ js
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SECRET_ID 
+  id: process.env.d8ef322f575a4b1cab842b1ea33b46e5,
+  secret: process.env.a786b31ea58a4c24bdd4134d61d5df24 
 };
